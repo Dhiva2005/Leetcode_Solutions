e63424885704697590367dd1450138f372fc051c
Dhivakar B <136208215+Dhiva2005@users.noreply.github.com>
2026-09-05 00:35:05 +0530

    Time: 38 ms (42.26%), Space: 47 MB (59.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hash = new HashMap<>();
         int n = nums.length;
-        if(n==0){
-            return new int[]{};
-        }
         for(int i=0;i<n;i++){
-            int find = target - nums[i];
-            if(hash.containsKey(find)){
-                return new int[]{hash.get(find),i};
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
             }
-            hash.put(nums[i],i);
         }
         return new int[]{};
     }
